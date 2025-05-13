53492692647d4b6f2403fbd7adff87782ae1a382
Jakub.Dragowski <kuba.dragowski@gmail.com>
2025-05-13 21:51:48 +0200

    feat: Integrate loaders for About and Home components with styling adjustments

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -10,6 +10,7 @@ import {
   faReact,
   faSass,
 } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 export const About: React.FC = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -35,52 +36,56 @@ export const About: React.FC = () => {
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass} // Provide a valid CSS class name
-            strArray={strArray}
-            idx={10}
-          />
-        </h1>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass} // Provide a valid CSS class name
+              strArray={strArray}
+              idx={10}
+            />
+          </h1>
 
-        <p>
-          I'm a very ambitious front-end developer looking for a role in an
-          established IT company with the opportunity to work with the latest
-          technologies on challenging and diverse projects.
-        </p>
-        <p>
-          I'm quiet confident, naturally curious, and perpetually working on
-          improving my chops one design problem at a time.
-        </p>
-        <p>
-          If I need to define myself in one sentence that would be a family
-          person, father of a beautiful daughter, a sports fanatic, photography
-          enthusiast, and tech-obsessed!!!
-        </p>
-      </div>
+          <p>
+            I'm a very ambitious front-end developer looking for a role in an
+            established IT company with the opportunity to work with the latest
+            technologies on challenging and diverse projects.
+          </p>
+          <p>
+            I'm quiet confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a beautiful daughter, a sports fanatic,
+            photography enthusiast, and tech-obsessed!!!
+          </p>
+        </div>
 
-      <div className="main-container">
-        <div className="stage-cube-cont">
-          <div className="sub-container">
-            <div className="sky">
-              <div className="stars"></div>
-              <div className="stars2"></div>
-              <div className="stars3"></div>
-              <div className="comet"></div>
-            </div>
-          </div>
-
-          <div className="cube-spinner">
-            {cubeFaces.map((face, index) => (
-              <div key={index} className={face.className}>
-                <FontAwesomeIcon icon={face.icon} />
+        <div className="main-container">
+          <div className="stage-cube-cont">
+            <div className="sub-container">
+              <div className="sky">
+                <div className="stars"></div>
+                <div className="stars2"></div>
+                <div className="stars3"></div>
+                <div className="comet"></div>
               </div>
-            ))}
+            </div>
+
+            <div className="cube-spinner">
+              {cubeFaces.map((face, index) => (
+                <div key={index} className={face.className}>
+                  <FontAwesomeIcon icon={face.icon} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Loader type="cube-transition" active />
+    </>
   );
 };
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,6 +5,7 @@ import { Logo } from './Logo/Logo';
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import { AnimatedLetters } from '../AnimatedLetters/AnimatedLetters';
+import Loader from 'react-loaders';
 
 export const Home: React.FC = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -30,40 +31,43 @@ export const Home: React.FC = () => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={strIntroArray}
-            idx={10}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={strIntroArraySecond}
-            idx={13}
-          />
-          <img src={logoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={strNameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={strJobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Front-end Developer / HTML/SCSS / JAVASCRIPT/REACT</h2>
-        <Link to="/contact" className="flat-button">
-          Contact Me
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={strIntroArray}
+              idx={10}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={strIntroArraySecond}
+              idx={13}
+            />
+            <img src={logoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={strNameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={strJobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front-end Developer / HTML/SCSS / JAVASCRIPT/REACT</h2>
+          <Link to="/contact" className="flat-button">
+            Contact Me
+          </Link>
+        </div>
+        <div></div>
+        <Logo />
       </div>
-      <div></div>
-      <Logo />
-    </div>
+      <Loader type="cube-transition" active />
+    </>
   );
 };
